8b62b6c094d8e195c05457879eec1e06cce0d262
Aidar Nurmukhametov <aidar.nurmukhametov@devexpress.com>
2023-11-13 12:18:02 +0400

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ObsoletesBug;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ ViewWithObsolete.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ObsoletesBug;
+
+public class ViewWithObsolete : Label {
+    [Obsolete($"Use {nameof(TextWithoutObsoleteProperty)} instead.", true)]
+    public static readonly BindableProperty TextWithObsoleteProperty;
+    [Obsolete($"Use {nameof(TextWithoutObsolete)} instead.", true)]
+    public string TextWithObsolete { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public static readonly BindableProperty TextWithoutObsoleteProperty = BindableProperty.Create(nameof(TextWithoutObsolete), typeof(string), typeof(ViewWithObsolete), propertyChanged: (s, _, _) => ((ViewWithObsolete)s).TextChanged?.Invoke(s, EventArgs.Empty));
+    public string TextWithoutObsolete { get => (string)GetValue(TextWithoutObsoleteProperty); set => SetValue(TextWithoutObsoleteProperty, value); }
+
+    [Obsolete($"Use {nameof(TextChanged)} instead.", true)]
+    public event EventHandler TextChangedObsolete {
+        add => throw new NotImplementedException();
+        remove => throw new NotImplementedException();
+    }
+
+    public event EventHandler TextChanged;
+}
+
